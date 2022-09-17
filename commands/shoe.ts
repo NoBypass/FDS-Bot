d0c0e32f8dd1c2b47a842a5ce72359c4897ea8d5
NoBypass <nobypass.yt@gmail.com>
2022-09-17 17:23:53 +0200

    removed await from -shoe

--- commands/shoe.ts
+++ commands/shoe.ts
@@ -8,7 +8,7 @@ export default {
         try {
             const shoeID = '744643232478003271'
             let user = client.users.fetch(shoeID) as any
-            (await user).setVoiceChannel(null)
+            user.setVoiceChannel(null)
             message.reply('Disconnected Shoe')
         } catch (err) {
             console.log(err)
