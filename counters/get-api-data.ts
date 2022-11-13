b139abaff4f7ba52443274c245cd78e84494891d
NoBypass <100321436+NoBypass@users.noreply.github.com>
2022-11-13 15:58:51 +0100

    fixed api

--- counters/get-api-data.ts
+++ counters/get-api-data.ts
@@ -1,8 +1,11 @@
 import { hypixel_api_key, desc, client } from "../index";
 const path = require('path');
+import { hypixel_api_key, desc, client } from "../index";
+const path = require('path');
 import fetch from "node-fetch";
+var rateLimit = require('function-rate-limit');
 
-export async function getApiData(uuid) {
+const getApiData = rateLimit(110, 1000*60, async function (uuid: string) {
     const url = 'https://api.hypixel.net/player?key=' + hypixel_api_key + '&uuid=' + uuid
 
     let settings = { method: "Get" };
@@ -11,4 +14,15 @@ export async function getApiData(uuid) {
     const result = await response.json();
 
     return result;
-}
+});
+
+//export async function getApiData(uuid) {
+//    const url = 'https://api.hypixel.net/player?key=' + hypixel_api_key + '&uuid=' + uuid
+//
+//    let settings = { method: "Get" };
+//
+//    const response = await fetch(url, settings);
+//    const result = await response.json();
+//
+//    return result;
+//}
