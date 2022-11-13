f7e6ef33f8a00374e34aa6482f7bf4da4e087525
NoBypass <100321436+NoBypass@users.noreply.github.com>
2022-11-13 16:50:30 +0100

    reverted rate limit

--- counters/get-api-data.ts
+++ counters/get-api-data.ts
@@ -1,7 +1,7 @@
 import { hypixel_api_key, desc, client } from "../index";
 const path = require('path');
 import fetch from "node-fetch";
-
+/*
 var rateLimit = require('function-rate-limit');
 
 export const getApiData = rateLimit(110, 1000*60, async function (uuid: string) {
@@ -13,8 +13,8 @@ export const getApiData = rateLimit(110, 1000*60, async function (uuid: string) 
     const result = await response.json();
 
     return result;
-});
-/*
+});*/
+
 export async function getApiData(uuid) {
     const url = 'https://api.hypixel.net/player?key=' + hypixel_api_key + '&uuid=' + uuid
 
@@ -24,4 +24,4 @@ export async function getApiData(uuid) {
     const result = await response.json();
 
     return result;
-}*/
+}
