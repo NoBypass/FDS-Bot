efdc5164bd82e0e2d22216591baaf872e26dd2df
NoBypass <nobypass.yt@gmail.com>
2022-11-28 12:11:44 +0100

    fixed non-boosters using -setcolor

--- commands/setcolor.ts
+++ commands/setcolor.ts
@@ -4,7 +4,7 @@ import { guild } from "../index";
 
 export default {
     callback: async (message: Message, ...args: string[]) => {
-        if (!message.member.permissions.has('Server Booster' as PermissionResolvable)) return descEmbed('You don\'t have permission to use this command.', message)
+        if (message.member.roles.cache.some(role => role.name === 'Server Booster')) return descEmbed('You don\'t have permission to use this command.', message)
 
         function isHex (hex) {
             return typeof hex === 'string'
