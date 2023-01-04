4b6ccdf82354204f3de8f7b96e3f114f0b2f9b2b
NoBypass <nobypass.yt@gmail.com>
2023-01-04 21:08:09 +0100

    fixed verify

--- commands/test.ts
+++ commands/test.ts
@@ -9,14 +9,13 @@ import {getApiData} from "../counters/get-api-data";
 
 export default {
     callback: async (message: Message, ...args: string[]) => {
-        message.reply(message.member.user.tag)
         fetch('https://api.mojang.com/users/profiles/minecraft/' + args)
             .then(response => response.json())
             .catch(err => console.log(err))
             .then(async mdata => {
                 var uuid = (mdata as any).id
                 const data = await getApiData(uuid) as any
-                message.reply(data?.socialMedia?.links?.DISCORD)
+                message.reply(message.member.user.tag + ' ' + data?.player?.socialMedia?.links?.DISCORD)
             })
     }
 }
