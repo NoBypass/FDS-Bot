c3ee3d17149f3e8b73772f849490596b224ba02a
NoBypass <nobypass.yt@gmail.com>
2023-07-16 21:42:29 +0200

    Event logger

--- src/handlers/event.ts
+++ src/handlers/event.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js'
+import { CacheType, Client, CommandInteraction } from 'discord.js'
 import { join } from 'path'
 import { BotEvent } from '../types/discord'
 import { log } from 'console'
@@ -15,7 +15,28 @@ module.exports = (client: Client) => {
         ? client.once(event.name, (...args: unknown[]) =>
             event.execute(...args),
           )
-        : client.on(event.name, (...args: unknown[]) => event.execute(...args))
+        : client.on(event.name, (...args: unknown[]) => {
+            switch (event.name) {
+              case 'interactionCreate':
+                console.log(
+                  chalk.yellow.bold('Interaction fired: ') +
+                    chalk.yellow.bold.underline(
+                      (args[0] as CommandInteraction).user.username,
+                    ) +
+                    chalk.yellow.bold(' with args: ') +
+                    chalk.yellow.bold.underline(args),
+                )
+                break
+              default:
+                console.log(
+                  chalk.yellow.bold('Event fired: ') +
+                    chalk.yellow.bold.underline(event.name) +
+                    chalk.yellow.bold(' with args:') +
+                    chalk.yellow.bold.underline(args),
+                )
+            }
+            event.execute(...args)
+          })
 
       log(chalk.green(`Event '${chalk.yellow.bold(event.name)}' registered`))
     },
